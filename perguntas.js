cf9052ed839d0710b8c874694fd01d54f6867947
Piovesan-0707 <magalhaesnatalia464@gmail.com>
2024-11-27 09:14:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Raphael Montes',
+    'No livro "Em Algum Lugar Nas Estrelas", quem são os protagonistas da história?',
+    'Os protagonistas são Early e Jack.'
+)
+
+criaCartao(
+    'Colleen Hoover',
+    'No livro "As Mil Partes Do Meu Coração",onde a protagonista mora?',
+    'Em uma igreja com sua família '
+)
+
+criaCartao(
+    'Jenny Han',
+    ' No livro "É assim que começa", como se chama a filha da protagonista?',
+    'Emerson Dory.'
+)
+
+criaCartao(
+    'Colleen Hoover',
+    ' No livro "É assim que acaba", o que faz com que Lily se apaixone por Ryle?',
+    'Lily se apaixona por Ryle devido à sua atração física, inteligência e confiança.'
+)
+
+criaCartao(
+    'Raphael Montes',
+    'Qual é o nome do autor do conto "O Coração Delator"?',
+    'Edgar Allan Poe.'
+)
+
+criaCartao(
+    'Hannah Nicole',
+    'Quem é a protagonista de "É assim que acaba"?',
+    'A protagonista do livro é a Lily Blossom uma jovem que trabalha em uma floricultura'
+)
